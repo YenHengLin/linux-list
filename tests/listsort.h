4ea3136b8bdb73400f9d85391730de820ab9f839
henry0929016816 <f74032112@mail.ncku.edu.tw>
2019-10-24 14:35:10 +0800

    Complete the merge sort conquer

--- tests/listsort.h
+++ tests/listsort.h
@@ -3,7 +3,7 @@
 static void list_mergesort(struct list_head *head)
 {
     struct list_head list_left, list_right;
-    struct listitem *item = NULL, *is = NULL;
+    struct listitem *iteml = NULL, *itemr = NULL, *is = NULL;
     struct list_head *halfp;
 
     if (list_empty(head) || list_is_singular(head))
@@ -29,8 +29,24 @@ static void list_mergesort(struct list_head *head)
     list_mergesort(&list_right);
     /* merge*/
     struct list_head *left_next, *right_next;
+    struct list_head *safel = NULL, *safer = NULL;
     left_next = list_left.next;
     right_next = list_right.next;
+    safel = left_next->next;
+    safer = right_next->next;
     while (left_next != &list_left && right_next != &list_right) {
+        iteml = list_entry(left_next, struct listitem, list);
+        itemr = list_entry(right_next, struct listitem, list);
+        if (iteml->i <= itemr->i) {
+            list_move_tail(&iteml->list, head);
+            left_next = safel;
+            safel = safel->next;
+        } else {
+            list_move_tail(&itemr->list, head);
+            right_next = safer;
+            safer = safer->next;
+        }
     }
+    list_splice_tail(&list_left, head);
+    list_splice_tail(&list_right, head);
 }
